e724499272ebf2941028891b93e146476ad90746
pzhang312 <pzhang312@ucla.edu>
2015-09-16 15:33:11 -0700

    define a function

--- my_first_script.R
+++ my_first_script.R
@@ -98,15 +98,29 @@ print(popu)
 plot(0:10, popu, xlab = "time", ylab = "size", col = 'blue')
 
 r <- 0.90
+NN = 100
+ttMAX = 10
 popu <- rep(NA, 11)
-popu[1] <- 100
-for(tt in 1:10){
+popu[1] <- NN
+for(tt in 1:ttMAX){
   popu[tt + 1] = popu[tt] * r;
   cat("The population size of year", tt, popu[tt + 1], "\n");
 }
 print(popu)
 plot(0:10, popu, xlab = "time", ylab = "size", col = 'red')
 
+
+#trying to make a function from the script above
+geometricGrowthFun <- function(r, NN, ttMAX){
+  popu <- rep(NA, 11)
+  popu[1] <- NN
+  for(tt in 1:ttMAX){
+    popu[tt + 1] = popu[tt] * r;
+    cat("The population size of year", tt, popu[tt + 1], "\n");
+  }
+  print(popu)
+  plot(0:ttMAX, popu, xlab = "time", ylab = "size", col = 'red')
+}
 
 
 
