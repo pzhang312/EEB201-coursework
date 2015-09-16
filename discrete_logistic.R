645cf6e935510bb49c7c7210fb11abf4a72fc27f
pzhang312 <pzhang312@ucla.edu>
2015-09-16 15:43:25 -0700

    logistic growth script

--- discrete_logistic.R
+++ discrete_logistic.R
@@ -1,10 +1,14 @@
-r <- 0.90
+popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK)) #equation
+
+
+r <- 1.05
 NN = 100
 ttMAX = 10
+KK = 1000
 popu <- rep(NA, 11)
 popu[1] <- NN
 for(tt in 1:ttMAX){
-  popu[tt + 1] = popu[tt] * r;
+  popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK));
   cat("The population size of year", tt, popu[tt + 1], "\n");
 }
 print(popu)
