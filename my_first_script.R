02562c71e3a1c199414679d219c91fe17c6fe111
pzhang312 <pzhang312@ucla.edu>
2015-09-16 12:30:41 -0700

    Sep.16 morning examples

--- my_first_script.R
+++ my_first_script.R
@@ -1,3 +1,89 @@
 library(ape)
 library(geiger)
 library(ggplot2)
+#trying in class exercise
+cat("hello world")
+getwd()
+setwd("/Users/ZhangXinhui/GitHub/EEB201-coursework")
+tips <- c("learn R", "avoid excel")
+source("/Users/ZhangXinhui/GitHub/EEB201-coursework/source.example.R")
+source("source.example.R")
+all.I.know.about.life.I.learned.in.grad.school()
+tt <- read.tree("tree.tre")
+str(tt)
+tt$tip.label
+attributes(tt)
+tt$Nnode
+dd <- read.table("data.txt", header = T, sep = '\t', as.is=T)
+dd <- read.table("data.txt", header=T, sep='\t', as.is=T)
+attributes(dd)
+dim(dd)
+dim(dd)[2]
+size <- runif(92)
+size <- runif(dim(dd)[1])
+head(cbind(dd,size))
+newdd <- cbind(dd, size)
+head(dd$species)
+newdd[1,]
+which(dd$mode == "MPF")
+dd[which(dd$mode == "MPF"), ]
+dd[which(dd$mode == "MPF"), which(dd$species == "Acanthurus_blochii")]
+mpfs <- which(dd$mode == 'MPF')
+mpf_swimmers <- dd[mpfs,]
+
+for (ii in 1:5){
+  cat("\nthe number is ", ii)
+}
+
+notfish <- c("bat", "dolphin", "toad", "soldier")
+for(a in notfish){
+  cat(a, "fish \n", sep="")
+}
+for(a in notfish){
+  cat(a, "fish", "1\n", sep="_")
+}
+for(a in notfish){
+  cat(a, "fish", " are tasty!\n", sep="")
+}
+
+xx <- 1
+while(xx < 5) {
+  xx <- xx+1; 
+  cat("value of xx is", xx, sep = "_", "\n")
+}
+print(xx)
+
+xx <- 1
+while(xx < 10) {
+  xx <- xx+1; 
+  #cat("value of xx is ", xx, ".", "\n", sep = "")
+  if (xx == 7) {
+    cat("My lucky number is", xx, "\n")
+  }
+  else if (xx == 2){
+    cat("The number", xx, "is unlucky.\n")
+  }
+  else {
+    cat("not exited about the number", xx, "\n")
+  }
+}
+print(xx)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
