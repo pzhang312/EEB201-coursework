a80bd98d50285b4ac8763d5d3c4a645da23593df
pzhang312 <pzhang312@ucla.edu>
2015-09-16 12:36:26 -0700

    Start Bootcamp Exercise

---
+++ Bootcamp Exercise 1.R
@@ -0,0 +1,11 @@
+#Exercise 4)
+years <- c( 2015, 2016, 2018, 2020, 2021)
+for(ii in 1:length(years)){
+  if(years[ii] %% 2 == 0){
+    cat(years[ii], 'Hooray, congressional elections!', sep = '\t', fill = T)
+  }
+  if((years[ii]-1792) %% 4 == 0){
+    cat(years[ii], " Hooray, presidential election!\n")
+  }
+  
+}
