b67f0dc16ec23a491426a3f1aba917672aa404c3
pzhang312 <pzhang312@ucla.edu>
2015-09-16 16:51:44 -0700

    different r values

--- discrete_logistic.R
+++ discrete_logistic.R
@@ -13,7 +13,7 @@ for(tt in 1:ttMAX){
 print(popu)
 plot(0:10, popu, xlab = "time", ylab = "size", col = 'red')
 
-
+#convert to a function
 discreteLogisticFun <- function(NN, r, KK, ttMAX){
   popu <- rep(NA, 11)
   popu[1] <- NN
@@ -21,7 +21,41 @@ discreteLogisticFun <- function(NN, r, KK, ttMAX){
     popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK));
   }
   print(popu)
+  plot(0:10, popu, xlab = "time", ylab = "size", col = "blue", type = "b")
 }
 
-discreteLogisticFun(10, 0.5, 100, 10)
+
+#different r values
+NN <- 10
+KK <- 100
+ttMAX <- 10
+rdVec <- c(-0.3, 0.3, 1.3, 1.9, 2.2, 2.7)
+par(mfrow = c(2,3))
+for(ii in 1:6){
+  discreteLogisticFun(NN, rdVec[ii], KK, ttMAX);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
