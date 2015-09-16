aec0e61a6c377b3ca9d07213a22e2f3ce41c05ce
pzhang312 <pzhang312@ucla.edu>
2015-09-16 15:58:48 -0700

    discrete logistic function

--- discrete_logistic.R
+++ discrete_logistic.R
@@ -9,7 +9,19 @@ popu <- rep(NA, 11)
 popu[1] <- NN
 for(tt in 1:ttMAX){
   popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK));
-  cat("The population size of year", tt, popu[tt + 1], "\n");
 }
 print(popu)
 plot(0:10, popu, xlab = "time", ylab = "size", col = 'red')
+
+
+discreteLogisticFun <- function(NN, r, KK, ttMAX){
+  popu <- rep(NA, 11)
+  popu[1] <- NN
+  for(tt in 1:ttMAX){
+    popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK));
+  }
+  print(popu)
+}
+
+discreteLogisticFun(10, 0.5, 100, 10)
+
