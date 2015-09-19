175535721710960da9734cb81b53de90438e29af
pzhang312 <pzhang312@ucla.edu>
2015-09-18 21:11:37 -0700

    Exercise 2, 3, 4

--- Bootcamp Exercise 1.R
+++ Bootcamp Exercise 1.R
@@ -9,16 +9,29 @@ for(ii in 1:9){
   }
 }
 
+#Exercise 2)
 
+for(ii in 1:19){
+  if(ii %% 2 == 0){
+    cat("&")
+  }
+  if(ii %% 2 != 0){
+    cat("*")
+  }
+}
 
+#Exercise 3)
 
-
-
-
-
+dogs(at the start) = 10
+dogs(at the end)= 15
+meatloaf(at the start) = 0
+meatloaf(at the end) = -30
+bubbles(at the start) = 12
+bubbles(at the end) = -4
 
 
 #Exercise 4)
+
 years <- c( 2015, 2016, 2018, 2020, 2021)
 for(ii in 1:length(years)){
   if(years[ii] %% 2 == 0){
@@ -29,3 +42,24 @@ for(ii in 1:length(years)){
   }
   
 }
+
+#Exercise 5)
+
+bankAccounts <- c(10, 9.2, 5.6, 3.7, 8.8, 0.5);
+interestRate <- 0.0125;
+compounded <- rep(NA, 6)
+for (i in 1:length(bankAccounts)) {
+  compounded[i] <- interestRate*bankAccounts[i] + bankAccounts[i]; }
+
+
+
+
+
+
+
+
+
+
+
+
+
