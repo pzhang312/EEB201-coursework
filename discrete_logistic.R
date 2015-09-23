638d8f28a79d9c92aca0a298e762c65da70f208b
pzhang312 <pzhang312@ucla.edu>
2015-09-23 14:32:21 -0700

    discrete_logistic exercises

--- discrete_logistic.R
+++ discrete_logistic.R
@@ -1,19 +1,28 @@
 popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK)) #equation
 
-
-r <- 1.05
-NN = 100
-ttMAX = 10
-KK = 1000
-popu <- rep(NA, 11)
-popu[1] <- NN
+#3.2.1 
+N0 <- 25
+RR <- 1.05
+ttMAX <- 100
+NN <- matrix(NA, nrow = 1, ncol = ttMAX + 1)
+NN[1] <- N0
 for(tt in 1:ttMAX){
-  popu[tt + 1] = popu[tt] * (1 + r * (1-popu[tt]/KK));
+  NN[tt + 1] <- RR*NN[tt]
 }
-print(popu)
-plot(0:10, popu, xlab = "time", ylab = "size", col = 'red')
+plot(1:(ttMAX+1), NN, xlab = "time", ylab = "N", type = "b", col = "blue")
 
-#convert to a function
+#3.2.2 convert to a function
+geometricGrowthFun <- function(N0, RR){
+  ttMAX <- 100
+  NN <- matrix(NA, nrow = 1, ncol = ttMAX + 1)
+  NN[1] <- N0
+  for(tt in 1:ttMAX){
+    NN[tt + 1] <- RR*NN[tt]
+  }
+  plot(1:(ttMAX+1), NN, xlab = "time", ylab = "N", type = "b", col = "blue")
+}
+
+#3.3.1 convert to a function
 discreteLogisticFun <- function(NN, r, KK, ttMAX){
   popu <- rep(NA, 11)
   popu[1] <- NN
@@ -25,7 +34,7 @@ discreteLogisticFun <- function(NN, r, KK, ttMAX){
 }
 
 
-#different r values
+#3.3.2 different r values
 NN <- 10
 KK <- 100
 ttMAX <- 10
@@ -35,10 +44,18 @@ for(ii in 1:6){
   discreteLogisticFun(NN, rdVec[ii], KK, ttMAX);
 }
 
-
-
-
-
+#4.1.1 
+expGrowthODE <- function(tt, yy, pars){
+  derivs <- pars['rr'] * yy
+  return(list(derivs))
+}
+output <- lsoda(init, tseq, expGrowthODE, pars)
+init <- 1
+tseq <- seq(0, 20, by = 0.01)
+pars <- c(rr = 0.1)
+expGrowthOutput <- lsoda(init, tseq, expGrowthODE, pars)
+head(expGrowthOutput)
+plot(expGrowthOutput[, 1], expGrowthOutput[, 2], col = 'red')
 
 
 
