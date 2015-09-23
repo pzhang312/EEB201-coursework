05835ba6a995a14049444feed59401f7ce276b8e
pzhang312 <pzhang312@ucla.edu>
2015-09-23 14:31:44 -0700

    Exercise 5-9

--- Bootcamp Exercise 1.R
+++ Bootcamp Exercise 1.R
@@ -51,12 +51,76 @@ compounded <- rep(NA, 6)
 for (i in 1:length(bankAccounts)) {
   compounded[i] <- interestRate*bankAccounts[i] + bankAccounts[i]; }
 
+#Exercise 6)
+
+bankAccounts <- c(10, 9.2, 5.6);
+interestRate <- 0.0525;   
+house <- c(4.8, 3.8, 5.7); 
+food<- c(3.5, 4.3, 5.0);    
+fun <- c(7.8, 2.1, 10.5);  
+income <- c(21, 21, 21); 
+
+for (j in 1:5) {
+  for (i in 1:length(bankAccounts)) {
+    bankAccounts[i] = bankAccounts[i] - house[i] - food[i] - fun[i] + income[i] 
+    bankAccounts[i] = bankAccounts[i]*interestRate + bankAccounts[i] 
+  }
+}
+print(bankAccounts)
+
+#Exercise 7)
 
 
+bankAccounts <- c(10, 9.2, 5.6);
+interestRate <- 0.0525;   
+house <- c(4.8, 3.8, 5.7); 
+food<- c(3.5, 4.3, 5.0);    
+fun <- c(7.8, 2.1, 10.5);  
+income <- c(21, 21, 21); 
 
+for (j in 2015:2020) {
+  if ((j - 2014) %% 2 == 0){
+    for (i in 1:length(bankAccounts)) {
+      bankAccounts[i] = bankAccounts[i] - house[i] - food[i] - fun[i] + income[i] 
+      bankAccounts[i] = bankAccounts[i]*interestRate + bankAccounts[i] 
+      cat("In the year of", j, "student", i, "will have a compound of", bankAccounts[i], ".\n");
+    }
+  }
+  if ((j - 2014) %% 2 != 0) {
+    for (i in 1:length(bankAccounts)){
+      if (i == 2){
+        bankAccounts[i] = bankAccounts[i] - house[i] - food[i] - fun[i] + income[i] 
+        bankAccounts[i] = bankAccounts[i]*interestRate + bankAccounts[i]
+      }
+      if (i != 2){
+        bankAccounts[i] = bankAccounts[i] - house[i] - food[i] - fun[i] + income[i] + 5; 
+        bankAccounts[i] = bankAccounts[i]*interestRate + bankAccounts[i]
+      }
+      cat("In the year of", j, "student", i, "will have a compound of", bankAccounts[i], ".\n");
+    }
+  }
+}
 
+#Exercise 8)
+xx <- 1
+sum = 0
+while(xx < 18){
+  sum = sum + xx;
+  xx = xx + 1;
+}
+print(sum)
 
-
+#Exercise 9)
+input = -100
+if(input <= -1){
+  print("small")
+}
+if(input >= 1){
+  print("big")
+}
+if(input >= -1 & input <= 1){
+  print("medium")
+}
 
 
 
